cd634238fc39a1b52318a3c5b79c91d892b7e6af
Mikoshiba-Kyu <mikoshibakyu@gmail.com>
2024-06-28 00:41:49 +0000

    add playwright

--- e2e/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "vitest";
-import { sampleFunction } from "../src/backend/serverFunctions/index";
-
-test("If 1 is given, 2 is returned.", () => {
-  expect(sampleFunction(1)).toBe(2);
-});
